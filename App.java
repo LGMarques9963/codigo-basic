5d9cee144d21e62e16bf2bad378e9f24e39123cc
Lorran Marques <lorran9963@gmail.com>
2022-10-26 19:20:08 -0300

    refactor1

--- App.java
+++ App.java
@@ -1,17 +1,10 @@
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.lang.reflect.Array;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
-import java.util.LinkedList;
 import java.util.Scanner;
 import java.util.TreeMap;
 
@@ -26,12 +19,17 @@ public class App {
         Path path2 = Paths.get(nameComplete);
         TreeMap<Integer, String[]> map = new TreeMap<Integer, String[]>();
         try (Scanner sc = new Scanner(Files.newBufferedReader(path2, Charset.defaultCharset()))) {
+            
+            int c = 10;
+
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
                 String[] tokens = line.split(" ");
-                // TRATAR A LINHA LIDA AQUI !!
+                int chave = Integer.parseInt(tokens[0]);
+                tokens[0] = Integer.toString(c);
+                c+=10;
                 System.out.println(tokens[0].toString());
-                map.put(Integer.parseInt(tokens[0]), tokens);
+                map.put(chave, tokens);
             }
             System.out.println(map);
             writeProgram(map);
@@ -42,22 +40,21 @@ public class App {
     }
 
     public static void writeProgram(TreeMap<Integer, String[]> input) throws IOException {
+        
         BufferedWriter writer = new BufferedWriter(new FileWriter("arquivo-rn.bas"));
-        int c = 10;
         for (var item : input.entrySet()) {
-            item.getValue()[0] = Integer.toString(c);
-            c += 10;
-        }
-        for (var item : input.entrySet()) {
-            int linha = Integer.parseInt(item.getValue()[0]);
             String concat = "";
             for (int i = 0; i < item.getValue().length; i++) {
-                concat=concat + item.getValue()[i] + " ";
-                if(item.getValue()[i].equals("gosub") || item.getValue()[i].equals("goto")){
-                    item.getValue()[i+1] = input.get(Integer.parseInt(item.getValue()[i+1]))[0];
+                String comando = item.getValue()[i];
+                concat += comando + " ";
+                if (comando.equals("gosub") || comando.equals("goto")) {
+
+                    int comandoSeguinte = Integer.parseInt(item.getValue()[i + 1]);
+                    item.getValue()[i + 1] = input.get(comandoSeguinte)[0];
                 }
             }
             System.out.println(concat);
+            writer.write(concat+"\n");
         }
         writer.close();
     }
@@ -66,7 +63,7 @@ public class App {
 /*
  * String frase = "";
  * for(int i =0; i < item.getValue().length; i++){
- * frase += new String(item.getValue()[i]+" ");
+ * frase += new String(comando+" ");
  * if(i == item.getValue().length-1){
  * frase += "\n";
  * }
